9130f5ae9da8ffbca1732aa862ef42e93e081942
kimyega <kimyega@naver.com>
2025-09-05 12:54:07 +0900

    basic

--- ex10-08.py
+++ ex10-08.py
@@ -9,13 +9,12 @@ def myFunc() :
     else:
         label1.configure(text= "Java")
 
-
 var = IntVar()
-rb1 = Radiobutton(window, text="파이썬", variable= var, value= 1, command= myFunc)
-rb2 = Radiobutton(window, text="C++", variable= var, value= 2, command= myFunc)
-rb3 = Radiobutton(window, text="Java", variable= var, value= 3, command= myFunc)
+rb1 = Radiobutton(window, text= "파이썬", variable= var, value= 1, command= myFunc)
+rb2 = Radiobutton(window, text= "C++", variable= var, value= 2, command= myFunc)
+rb3 = Radiobutton(window, text= "Java", variable= var, value= 3, command= myFunc)
 
-label1 = Label(window, text= "선택한 언어 : ", fg= "red")
+label1 = Label(window, text= "선택한 언어", fg= "red")
 
 rb1.pack()
 rb2.pack()
