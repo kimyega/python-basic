930e5180e3b22a37d8d9a2b6ccea276b41ad57d8
kimyega <kimyega@naver.com>
2025-09-05 12:53:59 +0900

    basic

--- ex10-07.py
+++ ex10-07.py
@@ -6,13 +6,13 @@ window = Tk()
 def myFunc() :
     if chk.get() == 0 :
         messagebox.showinfo("", "체크버튼이 꺼졌어요.")
-    else :
-        messagebox.showinfo("", "체크버튼이 꺼졌어요.")
+    else:
+        messagebox.showinfo("", "체크버튼이 켜졌어요.")
 
 chk = IntVar()
-ch1 = Checkbutton(window, text= "클릭하세요.", variable= chk,
-                  command= myFunc)
+cb1 = Checkbutton(window, text= "클릭하세요", variable= chk,
+                  command=myFunc)
 
-ch1.pack()
+cb1.pack()
 
 window.mainloop()
