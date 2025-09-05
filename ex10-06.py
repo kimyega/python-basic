eed78a3ac6cf246264322de8de76132fd6c82ce0
kimyega <kimyega@naver.com>
2025-09-05 12:53:50 +0900

    basic

--- ex10-06.py
+++ ex10-06.py
@@ -1,19 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
-from PIL import Image, ImageTk
 
 def myFunc() :
-    messagebox.showinfo("강아지 버튼", "강아지가 귀엽죠? ^^")
+    messagebox.showinfo("강아지 버튼", "강아지가 귀엽죠? ^^7")
 
 window = Tk()
 
-img = Image.open("C:/pycham/dog.jpg")
-img = img.resize((150, 150))
-photo = ImageTk.PhotoImage(img)
-
-
+photo = PhotoImage(file= "gif/dog.gif")
 button1 = Button(window, image= photo, command= myFunc)
+
 button1.pack()
 
 window.mainloop()
+
 
