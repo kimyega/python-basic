47e50c062b4476217a5eb1e09252cb89e4e7f358
kimyega <kimyega@naver.com>
2025-09-24 13:48:21 +0900

    basic

--- ex10-15.py
+++ ex10-15.py
@@ -2,11 +2,10 @@ from tkinter import *
 
 def clickMouse(event) :
     txt = ""
-    if event.num == 1 :
+    if event.num == 1:
         txt += "마우스 왼쪽 버튼이 ("
-    elif event.num == 3 :
+    elif event.num == 3:
         txt += "마우스 오른쪽 버튼이 ("
-
     txt += str(event.y) + "," + str(event.x) + ")에서 클릭됨"
     label1.configure(text= txt)
 
@@ -17,5 +16,5 @@ label1 = Label(window, text= "이곳이 바뀜")
 
 window.bind("<Button>", clickMouse)
 
-label1.pack(expand= 1, anchor= CENTER)
+label1.pack(expand=1, anchor=CENTER)
 window.mainloop()
