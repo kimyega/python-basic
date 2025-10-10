da7a64ec675ce06562feef63284580d7edb3f76b
kimyega <kimyega@naver.com>
2025-10-10 11:21:44 +0900

    basic

--- ex10-03.py
+++ ex10-03.py
@@ -1,12 +1,11 @@
 from tkinter import *
-
 window = Tk()
 
-label1 = Label(window, text = "COOKBOKK~~Python을")
-label2 = Label(window, text= "열심히", font= ("궁서체", 30),
+label1 = Label(window, text= "COOKBOOK~~Python을")
+label2 = Label(window, text="열심히", font= ("궁서체", 30),
                fg= "blue")
 label3 = Label(window, text= "공부 중입니다.", bg= "magenta",
-               width= 20, height= 5, anchor=SE)
+               width=20, height=5, anchor=SE)
 
 label1.pack()
 label2.pack()
