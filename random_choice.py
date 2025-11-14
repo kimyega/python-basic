759dc45aa5e5f186242ac5f28712382a38d2b986
kimyega <kimyega@naver.com>
2025-11-14 11:09:42 +0900

    basic

---
+++ random_choice.py
@@ -0,0 +1,13 @@
+import random
+
+def random_pop(data):
+    number = random.choice(data)
+    data.remove(number)
+    return number
+
+if __name__ == "__main__":
+    data = [1,2,3,4,5]
+    print(random_pop(data))
+
+    random.shuffle(data)
+    print(data)
