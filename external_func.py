df2d03014a9b58e5aabb9332c2ccebbf49bfa143
kimyega <kimyega@naver.com>
2025-11-14 11:08:36 +0900

    basic

---
+++ external_func.py
@@ -0,0 +1,33 @@
+import shutil
+import glob
+import tempfile
+import time
+import calendar
+import random
+
+
+shutil.copy("src.txt", "dst.txt")
+
+print(glob.glob("C:/python/p*"))
+
+filename = tempfile.mktemp()
+print(filename)
+f = tempfile.TemporaryFile()
+f.close()
+
+print(time.time())
+print(time.localtime(time.time()))
+print(time.asctime(time.localtime(time.time())))
+print(time.ctime())
+print(time.strftime('%x', time.localtime(time.time())))
+print(time.strftime('%c', time.localtime(time.time())))
+
+print(calendar.calendar(2015))
+print(calendar.prcal(2015))
+print(calendar.prmonth(2015, 12))
+print(calendar.weekday(2015, 12, 31))
+print(calendar.monthrange(2015, 12))
+
+print(random.random())
+print(random.randint(1, 10))
+print(random.randint(1, 55))
